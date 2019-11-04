c231283ee1b31904d170693c9ff13040e8416da5
Jarod Burchill <37840393+jarodburchill@users.noreply.github.com>
2019-11-04 14:21:19 -0500

    Added semicolon

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ function createWindow() {
   mainWindow.loadFile("index.html");
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on("closed", () => {
