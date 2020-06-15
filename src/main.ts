3e5cffff2d6b1282477270c16cdf9fd3fdfe3553
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-15 19:57:13 +0530

    don't listen to 'activate' until ready

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,15 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
@@ -30,12 +38,6 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
-});
-
-app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // In this file you can include the rest of your app"s specific main process
