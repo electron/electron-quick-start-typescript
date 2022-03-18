d894251ade9ea213081e46821dfff6cfe07df6d1
Erick Zhao <erick@hotmail.ca>
2022-03-18 13:31:27 -0700

    😟

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,6 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+import React from 'react';
+console.log(React);
