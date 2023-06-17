693d3557371cb9dd97d17335984608c0fa73f23c
CapitaineJSparrow <6690149+CapitaineJSparrow@users.noreply.github.com>
2023-06-17 20:44:50 +0200

    Update main.ts
    style: change function to arrow function

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", function () {
+  app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
